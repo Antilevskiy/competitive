7ed8c8c94234623aa902d42299cfafbe36be4fd4
Antilevskiy <2045vanya@gmail.com>
2021-05-24 23:47:08 +0300

    Fix something in cartesianTree

--- cartesianTree.cpp
+++ cartesianTree.cpp
@@ -7,27 +7,30 @@ template < typename T >
 struct Node 
 {
   T key;
-  long prior;
+  long long prior;
   Node* left;
   Node* right;
 };
 
 long genRand()
 {  
-   return rand() * 32786 + rand();
+   return rand() * 32768 + rand();
 }
 
 template < typename T >
-Node<T>* newNode(T key)
+Node<T>* newNode(const T& k)
 {
-	Node<T> newNode = new Node{key, genRand(), nullptr, nullptr};
+	Node<T>* newNode = new Node<T>{k, genRand(), nullptr, nullptr};
    	
    	return newNode;
 }
 
+Node<int> *root = 0;
+
 template < typename T >
-bool searchIterative(Node<T>* froot, int key)
+bool searchIterative(Node<T>* froot, const T& key)
 {
+
 	while(froot)
 	{
 		if(froot->key == key)
@@ -48,25 +51,52 @@ bool searchIterative(Node<T>* froot, int key)
 }  
 
 template < typename T >
-pair< Node<T>*, Node<T>* > split(Node<T>* froot, T key) 
+pair< Node<T>* , Node<T>* > split(Node<T>* froot, const T& k) 
 {
-	cout << "hello";
+	if(froot == nullptr)
+	{
+		return make_pair(nullptr, nullptr);
+	}
+	if(froot->key <= k)
+	{
+		
+		pair< Node<T>*, Node<T>* > p = split(froot->right, k);
+		froot->right = p.first;
+		return make_pair(froot, p.second);
+	}
+	else
+	{
+		pair< Node<T>*, Node<T>* > p = split(froot->left,k);
+		froot->left = p.second;
+		return make_pair(p.first, froot);
+	}
+}
+
+template < typename T > 
+void printTree(Node<T>* froot)
+{
+	if(froot != nullptr)
+    {
+		printTree<T>(froot->left);
+		cout << froot->key << " " ;
+		printTree<T>(froot->right);
+	}
 }
 
 template < typename T >
 Node<T>* mergeNodes(Node<T>* leftTree, Node<T>* rightTree)
 {
-	if(!leftTree)
+	if(leftTree == nullptr)
 	{
 		return rightTree;
 	}
 	
-	if(!rightTree)
+	if(rightTree == nullptr)
 	{
 		return leftTree;
 	}
 	
-	if(leftTree->priority < rightTree-> priority)
+	if(leftTree->prior < rightTree-> prior)
 	{
 		leftTree->right = mergeNodes(leftTree->right, rightTree);
          
@@ -81,28 +111,38 @@ Node<T>* mergeNodes(Node<T>* leftTree, Node<T>* rightTree)
 }
 
 template < typename T > 
-Node<T>* insert(Node<T>* froot, T key)
+Node<T>* insert(Node<T>* froot, const T& k)
 {
-	pair< Node<T>*, Node<T>* > p = split(froot, key);
-	Node<T>* node = newNode(key);
-	return mergeNodes(mregeNodes(p->first, node), p.second);
+	pair< Node<T>*, Node<T>* > p = split(froot, k);
+	Node<T>* node = newNode<T>(k);
+	return mergeNodes<T>(mergeNodes<T>(p.first, node), p.second);
 } 
 
 template < typename T>
-Node<T>* erase(Node<T>* froot, T key)
+Node<T>* erase(Node<T>* froot, const T& k)
 {
-    pair< Node<T>*, Node<T>* > p = split(froot, key);
-    pair< Node<T>*, Node<T>* > p2 = split(p.first, key - 1); //////////////CONCRETE 
+               pair< Node<T>*, Node<T>* > p = split(froot, k);
+    pair< Node<T>*, Node<T>* > p2 = split(p.first, k - 1); //
     
     delete p2.second;
     
     return mergeNodes(p2.first, p.second);
 }
 
-	Node<int> *root;
 
 int main()
 {	
-
+    for(int i = 0; i < 1000; i++)
+    {
+		root = insert(root, i);
+	}
+    //printTree(root);
+   
+    for(int i = 0; i < 1000; i++)
+    {
+		root = erase(root, i);
+	}
+	printTree(root);
+   
 	return 0;
 }
